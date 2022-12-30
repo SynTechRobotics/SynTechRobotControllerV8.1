1871934448a6e2126df4079e01275719fb4e96c0
Arugen09 <110001450+ftc20220522@users.noreply.github.com>
2022-12-29 20:34:09 -0600

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RoadRunnerRightSideCodeMeet3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RoadRunnerRightSideCodeMeet3.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.drive.opmode;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
@@ -16,7 +16,7 @@ import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
-@Autonomous
+@Autonomous(group="drive")
 public class RoadRunnerRightSideCodeMeet3 extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "FullCone1209.tflite";
 
